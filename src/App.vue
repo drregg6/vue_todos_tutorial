f792b8bd864b9109bb5553b7910b5365f27bdb91
Dave Regg <drregg6@gmail.com>
2019-10-08 16:17:49 -0400

    Added API link

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,8 @@ import Header from './components/layout/Header';
 import AddTodo from './components/AddTodo';
 import Todos from './components/Todos';
 
+import axios from 'axios';
+
 export default {
   name: 'app',
   components: {
@@ -20,23 +22,7 @@ export default {
   },
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: 'Todo One',
-          completed: false
-        },
-        {
-          id: 2,
-          title: 'Todo Two',
-          completed: false
-        },
-        {
-          id: 3,
-          title: 'Todo Three',
-          completed: false
-        }
-      ]
+      todos: []
     }
   },
   methods: {
@@ -48,6 +34,16 @@ export default {
     addTodo(newTodo) {
       this.todos = [...this.todos, newTodo];
     }
+  },
+  created() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then(res => {
+        return this.todos = res.data;
+      })
+      .catch(err => {
+        console.error(err.message);
+      });
   }
 }
 </script>
